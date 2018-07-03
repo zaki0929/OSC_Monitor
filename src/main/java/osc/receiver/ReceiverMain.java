964561781d780f17b5d3172768bd04ccbf7ae123
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-07-03 15:33:41 +0900

    Add osc sender and listener

---
+++ src/main/java/osc/receiver/ReceiverMain.java
@@ -0,0 +1,36 @@
+package osc.receiver;
+import com.illposed.osc.*;
+import java.net.SocketException;
+import java.util.Date;
+
+public class ReceiverMain{
+  public static void main(String[] args){
+
+    OSCPortIn receiver = null;
+    try{
+      receiver = new OSCPortIn(8000);
+    }catch(SocketException e2){
+      e2.printStackTrace();
+    }
+
+    OSCListener helloListener = new OSCListener(){
+      public void acceptMessage(Date time, OSCMessage message){
+        System.out.println("hello received!");
+        System.out.println(message.getAddress());
+        for(Object ob : message.getArguments()){
+          System.out.println((String) ob);
+        }
+      }
+    };
+
+    receiver.addListener("/greet/hello", helloListener);
+    
+    while(true){
+      receiver.startListening();
+      try{
+        Thread.sleep(1000);
+      }catch (InterruptedException e){
+      }
+    }
+  }
+}
---
+++ src/main/java/osc/sender/SenderMain.java
@@ -0,0 +1,32 @@
+package osc.sender;
+import com.illposed.osc.*;
+import java.net.*;
+import java.util.Date;
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class SenderMain{
+  public static void main(String[] args){
+
+    OSCPortOut sender = null;
+    try{
+      sender = new OSCPortOut(InetAddress.getLocalHost(), 8000);
+    }catch(Exception ex){
+      ex.printStackTrace();
+    }
+
+//    Collection<Object> chat = new Collection<Object>();
+    ArrayList<Object> chat = new ArrayList<Object>();
+    chat.add("Hello!!");
+
+    OSCMessage msg = new OSCMessage("/greet/hello", chat);
+    try{
+      sender.send(msg);
+      System.out.println("Sended!");
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    sender.close();
+  }
+}
