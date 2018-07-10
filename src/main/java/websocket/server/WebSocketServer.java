048720fa0cee732d795d94b06e0328abcb15102c
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-07-10 17:15:25 +0900

    Make the receiver work robustly & Clean build files

--- src/main/java/websocket/server/WebSocketServer.java
+++ src/main/java/websocket/server/WebSocketServer.java
@@ -36,12 +36,15 @@ public class WebSocketServer {
   public void echo(String message, Session session) {
     // クライアントから届いた指示が, Sender か Receiver のどちらへ向けたものか調べる
     String[] data = message.split(": ");
-    if(data[0].equals("s")){         // Sender への指示だった場合
-      sendOSC(data[1], session);              // OSC を送信するメソッドを実行
+    if(data[0].equals("s")){                      // Sender への指示だった場合
+      sendOSC(data[1], session);                  // OSC を送信するメソッドを実行
     }
-    if(data[0].equals("r")){         // Receiver への指示だった場合
+    if(data[0].equals("r")){                      // Receiver への指示だった場合
       if(!receiver.isRun){
         receiver.startReceive(data[1], session);  // OSC を受信するメソッドを実行
+      }else{
+        receiver.stopReceive(session);
+        receiver.startReceive(data[1], session);
       }
     }
   }
