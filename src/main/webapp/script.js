19f1aef9412d01ac07229532129145babd0e96f1
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-07-17 16:33:14 +0900

    Fix bug that crash occurs when the input is empty

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -7,13 +7,23 @@ $(function(){
     $('#log').prepend(receive.data + "<br/>");
   };
 
+  // 要素が空か否かを調べるプラグイン
+  (function($){
+    $.isBlank = function(obj){
+      return(!obj || $.trim(obj) === "");
+    };
+  })(jQuery);
+
 //---- Sender ---------------------------------------------
 
+  // OSC の Sender への命令を WebSocket サーバに送信する関数
   function ws_send_s(){
-    var address = $('#address_s').val();
-    var message = $('#message_s').val();
-    var data = 's: ' + address + ', ' + message;
-    ws.send(data);
+    if(!$.isBlank($('#address_s').val()) && !$.isBlank($('#message_s').val())){
+      var address = $('#address_s').val();
+      var message = $('#message_s').val();
+      var data = 's: ' + address + ', ' + message;
+      ws.send(data);
+    }
   };
 
   // send ボタンをクリックしたときに実行
@@ -35,30 +45,36 @@ $(function(){
 
 //---- Receiver ---------------------------------------------
 
+  // OSC の Receiver への命令を WebSocket サーバに送信する関数
   function ws_send_r(){
-    var address = $('#address_r').val();
-    var data = 'r: ' + address;
-    ws.send(data);
+    if(!$.isBlank($('#address_r').val())){
+      var address = $('#address_r').val();
+      var data = 'r: ' + address;
+      ws.send(data);
+    }
   };
 
+  // OSC の Receiver への命令を WebSocket サーバに送信する関数に
+　// 待機処理を加えた関数
   function ws_send_r_and_wait(){
-    if(isWait == 0){
+    if(!isWait && !$.isBlank($('#address_r').val())){
       ws_send_r(); 
       $('#receive').text("wait");
-      isWait = 1;
+      isWait = true;
       setTimeout(function(){
         $('#receive').text("receive");
-        isWait = 0;
-      },12000);
+        isWait = false;
+      },14000);
     }
   }
 
-  var isWait = 0;
+  // 待機状態か否かを明示する変数
+  var isWait = false;
 
-  // receive ボタンをクリックしたとき
+  // receive ボタンをクリックしたときに実行
   $('#receive').click(ws_send_r_and_wait);
 
-  // アドレス入力欄でエンターキーを押したとき
+  // アドレス入力欄でエンターキーを押したときに実行
   $('#address_r').keypress(function(e){
     if(e.which == 13){
       ws_send_r_and_wait();
